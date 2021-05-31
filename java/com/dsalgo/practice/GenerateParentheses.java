c1288c0b0762f49b2142ed21c756a8af77381459
Pijus Kumar Sarker <pijus.sarker@gmail.com>
2021-05-31 02:32:03 -0400

    Solved a problem related to generating parentheses

---
+++ java/com/dsalgo/practice/GenerateParentheses.java
@@ -0,0 +1,51 @@
+package com.dsalgo.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ *
+ * Example 1:
+ *
+ * Input: n = 3
+ * Output: ["((()))","(()())","(())()","()(())","()()()"]
+ * Example 2:
+ *
+ * Input: n = 1
+ * Output: ["()"]
+ *
+ * Solution: Backtracking
+ * We can do this by keeping track of the number of opening and closing brackets we have placed so far.
+ * We can start an opening bracket if we still have one (of n) left to place. And we can start a
+ * closing bracket if it would not exceed the number of opening brackets.
+ */
+public class GenerateParentheses {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        backtrack(ans, new StringBuilder(), 0, 0, n);
+        return ans;
+    }
+
+    public void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max){
+        if (cur.length() == max * 2) {
+            ans.add(cur.toString());
+            return;
+        }
+
+        if (open < max) {
+            cur.append("(");
+            backtrack(ans, cur, open+1, close, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+        if (close < open) {
+            cur.append(")");
+            backtrack(ans, cur, open, close+1, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+    }
+
+    public static void main(String args[]) {
+        GenerateParentheses obj = new GenerateParentheses();
+    }
+}
