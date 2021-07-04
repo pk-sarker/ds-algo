6d70867a921bd25d4870082965e6405e850cdf58
Pijus Kumar Sarker <pijus.sarker@gmail.com>
2021-07-04 03:36:28 -0400

    Added problems in lincked list section

---
+++ java/ds/LinkedList/FindMiddle.java
@@ -0,0 +1,42 @@
+package ds.LinkedList;
+
+import ds.Common.ListNode;
+
+public class FindMiddle {
+
+    public static int findMid(ListNode head) {
+
+        if (head == null) {
+            return -1;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow.val;
+    }
+
+    public static void printLL(ListNode head) {
+        ListNode cur =  head;
+        StringBuilder sb = new StringBuilder();
+
+        while(cur != null) {
+            sb.append(cur.val);
+            if (cur.next != null) {
+                sb.append("->");
+            }
+            cur = cur.next;
+        }
+        System.out.println(sb.toString());
+    }
+    public static void main(String args[]) {
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        printLL(head);
+        System.out.println("Middle: " + FindMiddle.findMid(head));
+
+    }
+}
--- java/ds/LinkedList/README.md
+++ java/ds/LinkedList/README.md
@@ -1,1 +1,6 @@
 # Linked List
+
+- [Reverse a LinkedList](./ReverseLinkedList.java)
+- [Find middle of a LinkedList](./FindMiddle.java)
+- [Check cycle in a LinkedList](./LinkedListCycle.java)
+- [Add two numbers](./AddNumber.java)
