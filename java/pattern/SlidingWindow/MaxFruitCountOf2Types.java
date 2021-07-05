a8e5337a72f91f41350b1e7115d9300ab86ba035
Pijus Kumar Sarker <pijus.sarker@gmail.com>
2021-07-05 15:31:26 -0400

    Adding problems in sliding window pattern

---
+++ java/pattern/SlidingWindow/MaxFruitCountOf2Types.java
@@ -0,0 +1,54 @@
+package pattern.SlidingWindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of characters where each character represents a fruit tree, you are given two baskets,
+ * and your goal is to put maximum number of fruits in each basket. The only restriction is that each
+ * basket can have only one type of fruit.
+ *
+ * You can start with any tree, but you can’t skip a tree once you have started. You will pick one
+ * fruit from each tree until you cannot, i.e., you will stop when you have to pick from a
+ * third fruit type.
+ *
+ * Write a function to return the maximum number of fruits in both baskets.
+ *
+ * Input: Fruit=['A', 'B', 'C', 'A', 'C']
+ * Output: 3
+ * Explanation: We can put 2 'C' in one basket and one 'A' in the other from the subarray ['C', 'A', 'C']
+ *
+ * Input: Fruit=['A', 'B', 'C', 'B', 'B', 'C']
+ * Output: 5
+ * Explanation: We can put 3 'B' in one basket and two 'C' in the other basket.
+ * This can be done if we start with the second letter: ['B', 'C', 'B', 'B', 'C']
+ *
+ */
+public class MaxFruitCountOf2Types {
+    public static int findLength(char[] arr) {
+        int windowStart = 0, maxLength = 0;
+        Map<Character, Integer> fruitFrequencyMap = new HashMap<>();
+        // try to extend the range [windowStart, windowEnd]
+        for (int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+            fruitFrequencyMap.put(arr[windowEnd], fruitFrequencyMap.getOrDefault(arr[windowEnd], 0) + 1);
+            // shrink the sliding window, until we are left with '2' fruits in the frequency map
+            while (fruitFrequencyMap.size() > 2) {
+                fruitFrequencyMap.put(arr[windowStart], fruitFrequencyMap.get(arr[windowStart]) - 1);
+                if (fruitFrequencyMap.get(arr[windowStart]) == 0) {
+                    fruitFrequencyMap.remove(arr[windowStart]);
+                }
+                windowStart++; // shrink the window
+            }
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Maximum number of fruits: " +
+                MaxFruitCountOf2Types.findLength(new char[] { 'A', 'B', 'C', 'A', 'C' }));
+        System.out.println("Maximum number of fruits: " +
+                MaxFruitCountOf2Types.findLength(new char[] { 'A', 'B', 'C', 'B', 'B', 'C' }));
+    }
+}
