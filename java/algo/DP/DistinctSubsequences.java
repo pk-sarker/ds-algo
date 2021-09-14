ada538a1249b3a330895a8d13f37ed51c8c06d50
Pijus Kumar Sarker <pijus.sarker@gmail.com>
2021-09-13 23:22:30 -0400

    Updated problemz

--- java/algo/DP/DistinctSubsequences.java
+++ java/algo/DP/DistinctSubsequences.java
@@ -81,9 +81,74 @@ public class DistinctSubsequences {
         return res;
     }
 
+    /**
+     * Dynamic Programming
+     *
+     * 1) Initialize a 2D array dp of size M×N where M represents the length of string S while N represents
+     * the length of string T.
+     * 2) An important thing to remember here is what recurseDP(i, j) actually represents. It basically
+     * represents the number of distinct subsequences in string s[i....M] that equals the string t[j...N].
+     * This is important because we will have our iterative loops based on this idea itself. This implies
+     * that we will first calculate the value of recurseDP(i, j) before we can find answers for recurseDP(i - 1, j)
+     * or recurseDP(i, j - 1) or recurseDP(i - 1, j - 1).
+     * 3) Based on this idea, we will have an outer loop for the index i which will go from M - 1 to 0 and
+     * an inner loop for j from N - 1 to 0.
+     * 4) We first handle our recursion's base case in outside of our nested loop and here we initialize the last column and the last row of our dp table.
+     *      - dp[M][j] represents that we have an empty substring left in String S while we still have T[j...N-1] left.
+     *          That means there is no possibility of match, so dp[M][j] = 0
+     *      - dp[i][N] represents that we have matched all the letter in T, and now at empty string, so dp[i][N] = 1
+     *
+     * 5) After that, we simply set dp[i][j] = dp[i + 1][j]. Remember that there was one recursive call that
+     *  we need to make irrespective of whether there is a character match or not?
+     *
+     * 6) Then we check if the characters s[i] and t[j] match or not. If they do, then we add dp[i + 1][j + 1] to dp[i][j].
+     *  In the recursion based solution, we were caching this value in the dictionary. Here, the dictionary is replaced by the dp array.
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public int numDistinctDP(String s, String t) {
+
+        int M = s.length();
+        int N = t.length();
+        // dp[i][j] = number of distinct subsequences in string s[i....M] that equals the string t[j...N]
+        int[][] dp = new int[M + 1][N + 1];
+
+        // Base case initialization
+        for (int j = 0; j <= N; j++) {
+            dp[M][j] = 0;
+        }
+
+        // Base case initialization
+        for (int i = 0; i <= M; i++) {
+            dp[i][N] = 1;
+        }
+
+        // Iterate over the strings in reverse so as to
+        // satisfy the way we've modeled our recursive solution
+        for (int i = M - 1; i >= 0; i--) {
+            for (int j = N - 1; j >= 0; j--) {
+
+                // Remember, we always need this result
+                dp[i][j] = dp[i + 1][j];
+
+                // If the characters match, we add the
+                // result of the next recursion call (in this
+                // case, the value of a cell in the dp table
+                if (s.charAt(i) == t.charAt(j)) {
+                    dp[i][j] += dp[i + 1][j + 1];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+
+
     public static void main(String args[]) {
         DistinctSubsequences obj = new DistinctSubsequences();
         System.out.println(obj.numDistinct("rabbbit", "rabbit"));
         System.out.println(obj.numDistinct("babgbag", "bag"));
+        System.out.println(obj.numDistinctDP("babgbag", "bag"));
     }
 }
