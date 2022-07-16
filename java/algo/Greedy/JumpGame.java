dd69d99014ed776c2b0a212a3137c95434522409
Pijus Kumar Sarker <pijus.sarker@gmail.com>
2022-07-16 10:20:10 +0600

    Updated solution doc for jump game

--- java/algo/Greedy/JumpGame.java
+++ java/algo/Greedy/JumpGame.java
@@ -5,6 +5,24 @@ package algo.Greedy;
  * and each element in the array represents your maximum jump length at that position.
  *
  * Return true if you can reach the last index, or false otherwise.
+ *
+ * We used Greedy approach to solve the Jump Game problem:
+ * The idea here is the scan the array from right to left and keep
+ * track of last known position from where we can reach/jump to
+ * the last index in the array.
+ *
+ * Example:
+ * Lets take an example: [2, 0, 4, 2, 1, 0, 2, 1]
+ * In the example last index, 7, and the value is 1. We can always reach to
+ * last index form the same position with 0 jump. So index/position 7 is a
+ * known position from where we can reach to last index.
+ *
+ * Now we scan to right, next position/index 6, the value is 2. With 2 at
+ * position 6 we can jump maximum 2 ahead, that mean we can reach to [7,8]
+ * indices. Here index 7 is already known as a good index.
+ * So, as we are moving from left to right, one we flow,
+ * from current index if we reach to a index which is known that we can reach
+ * to last index then we can reach from current index to last index.
  */
 public class JumpGame {
 
