031cf24ba13015f203da4d9f36665de82ea073b9
Pijus Kumar Sarker <pijus.sarker@gmail.com>
2025-07-27 22:31:03 -0400

    Updating some common class.

--- java/Common/ListNode.java
+++ java/Common/ListNode.java
@@ -1,13 +1,38 @@
 package Common;
 
+
 public class ListNode {
     public int val;
     public ListNode next;
-    public ListNode(int value){
+
+    public ListNode(int value) {
         this.val = value;
     }
-    public ListNode(int value, ListNode next){
+
+    public ListNode(int value, ListNode next) {
         this.val = value;
         this.next = next;
+    }
+
+    public static void printNodes(ListNode head) {
+        ListNode current = head;
+        StringBuilder sb = new StringBuilder();
+        while (current != null) {
+            sb.append(current.val);
+            if (current.next != null) sb.append(" -> ");
+            current = current.next;
+        }
+        System.out.println(sb.toString());
+    }
+    public static void printNodes(ListNode head, String printPrefix) {
+        ListNode current = head;
+        StringBuilder sb = new StringBuilder();
+        sb.append(printPrefix);
+        while (current != null) {
+            sb.append(current.val);
+            if (current.next != null) sb.append(" -> ");
+            current = current.next;
+        }
+        System.out.println(sb.toString());
     }
 }
--- java/Common/TreeNode.java
+++ java/Common/TreeNode.java
@@ -1,5 +1,7 @@
 package Common;
 
+import java.util.Stack;
+
 public class TreeNode {
     public int val;
     public TreeNode left;
@@ -7,18 +9,44 @@ public class TreeNode {
 
     public TreeNode() {
     }
+
     public TreeNode(int val) {
         this.val = val;
         this.left = null;
         this.right = null;
     }
+
     public TreeNode(int val, TreeNode left) {
         this.val = val;
         this.left = left;
     }
+
     public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    public static void pintTree(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        TreeNode node = root;
+        while (node != null || !stack.isEmpty()) {
+
+            // Traverse to the leftmost node, pushing nodes onto the stack
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            // Pop the top node (which is the current node in inorder sequence)
+            node = stack.pop();
+            sb.append("->");
+            sb.append(node.val);
+
+            // Visited the node and its left subtree.
+            // Now, it's right subtree's turn
+            node = node.right;
+        }
+        System.out.println(sb.toString());
     }
 }
